8079e27e08e9ef2cb28a77b729c444addd45c32a
Kang Ki Tae <kt.kang@ridi.com>
2017-04-19 15:35:41 +0900

    Make UrlHelper.

---
+++ src/Util/UrlHelper.php
@@ -0,0 +1,58 @@
+<?php
+namespace Ridibooks\Platform\Cms\Util;
+
+class UrlHelper
+{
+    /**
+     * @param string $url
+     * @param string $msg
+     * @return string
+     */
+    public static function printAlertRedirect($url, $msg)
+    {
+        $html = '<!doctype html><html><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1"></head><body><script>';
+        if (!empty($msg)) {
+            $html .= "alert(" . json_encode($msg) . ");";
+        }
+        $html .= "location.href=" . json_encode($url) . ";";
+        $html .= "</script></body></html>\n";
+
+        return $html;
+    }
+
+    /**
+     * @param string $msg
+     * @return string
+     */
+    public static function printAlertHistoryBack($msg)
+    {
+        $html = '<!doctype html><html><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1"></head><body><script>';
+        if (!empty($msg)) {
+            $html .= "alert(" . json_encode($msg) . ");";
+        }
+        $html .= "history.go(-1);";
+        $html .= "</script></body></html>\n";
+
+        return $html;
+    }
+
+
+    /**
+     * @param array $query_map
+     * @param array $replace
+     * @return string
+     */
+    public static function buildQuery($query_map, $replace)
+    {
+        foreach ($replace as $k => $v) {
+            $query_map[$k] = $v;
+        }
+
+        $query_string = http_build_query($query_map);
+        if (!empty($query_string)) {
+            $query_string = '?' . $query_string;
+        }
+
+        return $query_string;
+    }
+}
