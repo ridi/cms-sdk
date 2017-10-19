f42f80917619c64da1d5d7c720152fbf446f1581
Jihun Kim <devgrapher@ridi.com>
2017-10-19 19:34:43 +0900

    Use express-session

--- samples/js/index.js
+++ samples/js/index.js
@@ -1,23 +1,71 @@
 'use strict';
 
-require("babel-polyfill")
+require('babel-polyfill')
+const express = require('express')
+const session = require('express-session');
+const couchbaseStore = require('connect-couchbase')(session);
 
-const { CmsClient, UserAuth } = require('@ridibooks/cms-sdk');
+const app = express()
+const cbStore = new couchbaseStore({
+    bucket: "session",
+});
+
+const { CmsClient, UserAuth } = require('../../lib/js/dist');
+
 const cmsRpcUrl = 'http://localhost:8000';
 const client = new CmsClient(cmsRpcUrl);
-/*
-client.adminUser.getUser('admin', (err, result) => {
-    console.log('sync=', result);
-});
-*/
-
 const userAuth = new UserAuth();
 
-async function readAuth() {
-    const auth = await userAuth.readUserAuth(client, 'admin', true);
-    console.log(auth);
-    const has = await userAuth.hasUrlAuth(auth, '', '/super/logs');
-    console.log(has);
+async function readAuth(userId, sessionId) {
+    return await userAuth.readUserAuth(client, userId, true);
 }
 
-readAuth();
+async function authorizer(req, res, next) {
+    if (req.path === '/login') {
+        next();
+        return;
+    }
+
+    const auth = req.session.userAuth;
+    if (!auth) {
+        res.redirect(`/login`);
+        return;
+    }
+    const hasAuth = await userAuth.hasUrlAuth(auth, req.method, req.url);
+    console.log(`hasAuth: ${req.url}, ${hasAuth}`);
+    if (!hasAuth) {
+        res.sendStatus(403);
+        return;
+    }
+    next();
+}
+
+app.use(session({
+    // use the default PHP session cookie name
+    name: 'PHPSESSID',
+    secret: 'example',
+    store: cbStore,
+}));
+
+app.use(authorizer);
+
+app.get('/', function (req, res) {
+    // send menu items for the user.
+    res.json(req.session.userAuth.menus);
+});
+
+app.get('/login', function (req, res) {
+    console.log('Login');
+    readAuth('admin', req.session.id).then(function(data) {
+        req.session.userAuth = data;
+        res.redirect('/');
+    });
+});
+
+app.get('/super/logs', function (req, res) {
+    res.send('Restricted page');
+});
+
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+});
