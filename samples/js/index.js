7b73ed9c641280d6698c2808f5cf374e827809f8
Jihun Kim <devgrapher@ridi.com>
2017-11-27 15:05:37 +0900

    Add account status check in isLogin

--- samples/js/index.js
+++ samples/js/index.js
@@ -8,32 +8,37 @@ const sdk = new CmsSdk({
   couchbaseUri: '127.0.0.1',
 });
 
-function authorizer(req, res, next) {
+async function authorizer(req, res, next) {
   console.log(req.url)
-  // check login
-  const loginId = req.session.getLoginId();
-  if (loginId == null) {
+
+  const login = req.session && await req.session.isLogin();
+  if (!login) {
     const loginUrl = sdk.getLoginPageUrl(req.url);
     res.redirect(req.baseUrl + loginUrl);
-    return
+    return;
   }
-  sdk.accessMenu(loginI, req.method, req.url).then(allowed => {
-    if (allowed) {
-      console.log(`access allowed: ${req.url}`);
-      next();
-    } else {
-      res.sendStatus(403);
-    }
-  });
+
+  const loginId = req.session.getLoginId();
+  const allowed = await sdk.accessMenu(loginId, req.method, req.url);
+  if (allowed) {
+    console.log(`access allowed: ${req.url}`);
+    next();
+  } else {
+    res.sendStatus(403);
+  }
 }
 
-function cmsSession(req, res, next) {
+async function cmsSession(req, res, next) {
   const cookies = new Cookies(req, null);
   const sessionId = cookies.get('PHPSESSID');
-  req.session = new CmsSession(sessionId, sdk);
-  req.session.load().then(session => {
+  if (sessionId == null) {
     next();
-  });
+    return;
+  }
+
+  req.session = new CmsSession(sessionId, sdk);
+  await req.session.load();
+  next();
 }
 
 const app = express();
