7bbb88d8664ed4ab560da74daf33d7efb19217a3
Kang Ki Tae <kt.kang@ridi.com>
2018-04-03 21:58:36 +0900

    Move authorize logic to cms main server

--- lib/php/src/Auth/AdminAuthService.php
+++ lib/php/src/Auth/AdminAuthService.php
@@ -2,8 +2,14 @@
 
 namespace Ridibooks\Cms\Auth;
 
+use Ridibooks\Cms\Thrift\AdminAuth\AdminAuthServiceClient;
+use Ridibooks\Cms\Thrift\Errors\ExpiredTokenException;
+use Ridibooks\Cms\Thrift\Errors\MalformedTokenException;
+use Ridibooks\Cms\Thrift\Errors\NoTokenException;
+use Ridibooks\Cms\Thrift\Errors\UnauthorizedException;
 use Ridibooks\Cms\Thrift\ThriftService;
 use Ridibooks\Cms\Util\UrlHelper;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -33,26 +39,39 @@ class AdminAuthService
         return AdminUserService::getAdminUserTag(LoginService::GetAdminID());
     }
 
-    /**해당 URL에 접근할 권한이 있는지 검사한다.<br/>
-     * 문제점
-     * - 각 menu 밑에 sub url 검사를 한번 더 하는데 의존관계를 알기 힘들다.
-     * - 현재는 여러 페이지에서 사용하는 ajax_url의 권한을 확실하게 하지 못한다.
-     * - 나중에 권한을 좀 더 세분화 시킬때는 sub url을 unique키로 하여 각 sub url의 진입점을 구분하도록 메뉴주소를 따로 구분하는게 좋을것 같다.
-     * @param null $method
-     * @param null $check_url
-     * @throws
+    /**
+     * @param Request $request
+     * @return null|Response
      */
-    public static function hasUrlAuth($method = null, $check_url = null)
+    public static function authorize($request)
     {
-        if (class_exists('Config')) {
-            $is_dev = \Config::$UNDER_DEV;
-        } else {
-            $is_dev = $_ENV['DEBUG'];
+        try {
+            $token = LoginService::getAccessToken();
+
+            $client = ThriftService::getHttpClient('AdminAuth');
+            $client->authorize(
+                $token,
+                $request->getMethod(),
+                $request->getRequestUri()
+            );
+        } catch (NoTokenException $e) {
+            $redirect_url = '/authorize?return_url=' . urlencode($request->getRequestUri());
+            return RedirectResponse::create($redirect_url);
+        } catch (MalformedTokenException $e) {
+            $redirect_url = '/authorize?return_url=' . urlencode($request->getRequestUri());
+            return RedirectResponse::create($redirect_url);
+        } catch (ExpiredTokenException $e) {
+            $redirect_url = '/authorize?return_url=' . urlencode($request->getRequestUri());
+            return RedirectResponse::create($redirect_url);
+        } catch (UnauthorizedException $e) {
+            if ($request->isXmlHttpRequest()) {
+                return new Response($e->getMessage(), Response::HTTP_UNAUTHORIZED);
+            } else { //일반 페이지
+                return new Response(UrlHelper::printAlertHistoryBack($e->getMessage()));
+            }
         }
 
-        if (!self::hasHashAuth($method, $check_url) && !$is_dev) {
-            throw new \Exception("해당 권한이 없습니다.");
-        }
+        return null;
     }
 
     /**해당 URL의 Hash 권한이 있는지 검사한다.<br/>
@@ -66,6 +85,7 @@ class AdminAuthService
             $check_url = $_SERVER['REQUEST_URI'];
         }
 
+        /** @var AdminAuthServiceClient $client */
         $client = ThriftService::getHttpClient('AdminAuth');
         return $client->hasHashAuth($hash, $check_url, LoginService::GetAdminID());
     }
@@ -80,6 +100,7 @@ class AdminAuthService
             $check_url = $_SERVER['REQUEST_URI'];
         }
 
+        /** @var AdminAuthServiceClient $client */
         $client = ThriftService::getHttpClient('AdminAuth');
         $hash_array = $client->getCurrentHashArray($check_url, LoginService::GetAdminID());
 
@@ -93,33 +114,5 @@ class AdminAuthService
     {
         $admin = AdminUserService::getUser(LoginService::GetAdminID());
         return $admin && $admin['is_use'];
-    }
-
-    /**
-     * @param Request $request
-     * @return null|Response
-     */
-    public static function authorize($request)
-    {
-        if (!LoginService::isAuthRequired($request)) {
-            return null;
-        }
-
-        if (!LoginService::validateLogin($request)) {
-            return LoginService::createRedirectForLogin($request);
-        }
-
-        try {
-            self::hasUrlAuth(null, $request->getRequestUri());
-        } catch (\Exception $e) {
-            // 이상하지만 기존과 호환성 맞추기 위해
-            if ($request->isXmlHttpRequest()) {
-                return new Response($e->getMessage(), Response::HTTP_UNAUTHORIZED);
-            } else { //일반 페이지
-                return new Response(UrlHelper::printAlertHistoryBack($e->getMessage()), Response::HTTP_UNAUTHORIZED);
-            }
-        }
-
-        return null;
     }
 }
